f679b4c8fe8eba22042edeba9a3e7d49137b1529
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-09-05 16:15:32 -0600

    Concatenate Arrays

---
+++ concat.js
@@ -0,0 +1,8 @@
+const concat = (ar1, ar2) => {
+  return ar1.concat(ar2);
+};
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
