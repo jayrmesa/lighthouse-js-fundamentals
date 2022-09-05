0b4957005d16ee4d99f1e2bfe61b0b15f4ec5a42
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-09-05 16:10:33 -0600

    Looping - lastIndexOf

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = (array, num) => {
+  let lastIndex = -1;
+  array.forEach((element, index) => {
+    if (element === num) {
+      lastIndex = index;
+    }
+  });
+  return lastIndex;
+};
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
