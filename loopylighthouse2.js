97388f8d8e305b80a71fae183fa766adab89d608
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-09-05 16:43:49 -0600

    Loopy Lighthouse 2

---
+++ loopylighthouse2.js
@@ -0,0 +1,17 @@
+const loopyLighthouse = (range, multiples, words) => {
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (!(i % multiples[0]) && !(i % multiples[1])) {
+      console.log(words[0] + words[1]);
+    } else {
+      if (!(i % multiples[0])) {
+        console.log(words[0]);
+      } else if (!(i % multiples[1])) {
+        console.log(words[1]);
+      } else {
+        console.log(i);
+      }
+    }
+  }
+};
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
